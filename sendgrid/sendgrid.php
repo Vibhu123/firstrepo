6318f4d2cc02809d6475608406848ba2a46f4e10
Vibhu Tiwari <vibhutiwari321@gmail.com>
2013-02-25 13:15:08 +0530

    Update sendgrid/sendgrid.php

--- sendgrid/sendgrid.php
+++ sendgrid/sendgrid.php
@@ -1,22 +1,51 @@
 <?php
 include '/var/www/sendgrid-php/SendGrid_loader.php';
-$sendgrid=new SendGrid('anmolgagneja','30086269');
-$mail=new SendGrid\Mail();
-$email1=$_REQUEST['email'];
-$message=$_REQUEST['message'];
-$subject=$_REQUEST['subject'];
-$attach=$_FILES['file']['tmp_name'];
-$email=$_REQUEST['mr'];
-$name=$_REQUEST['nm'];
-$name1=explode(",",$name);
-$emails=explode(",",$email);
-$mail->setFrom($email1)->setSubject($subject)->setText($message)->addAttachment($attach);
-foreach($emails as $value)
-{ $mail->addTo($value);
-}
-foreach($name1 as $name2){
-$msg="Hello ".$name2." how are you";
-$mail->setHtml($msg);
-}
+
+$sendgrid = new SendGrid('anmolgagneja','30086269');
+
+$mail = new SendGrid\Mail();
+
+$mail->setFrom('vibhutiwari321@gmail.com')->
+
+setSubject("Heyy")->
+
+setText("Hi!!!!")->
+
+addTo('vibhutiwari321@gmail.com')->
+
+addTo('vibhutiwari92@yahoo.in')->
+
+addTo('vibhu@gmail.com')->
+
+addUniqueArgument('customer','somewhere')->
+
+setHtml("Hello %name% this is our first email")->
+
+addSubstitution("%name%",array("vibhu","vibhu"))->
+
+addFilterSetting("gravatar","enable",1)->
+
+addFilterSetting("clicktrack","enable",1)->
+
+addFilterSetting("footer","enable",1)->
+
+addFilterSetting("subscriptiontrack","enable",1)->
+
+addFilterSetting("subscriptiontrack","text/html","<%Unsubscribe Here%>")->
+
+addFilterSetting("opentrack","enable",1)->
+
+addFilterSetting("spamcheck","enable",1)->
+
+addFilterSetting("spamcheck","maxscore",5)->
+
+addFilterSetting("template","enable",1)->
+
+addFilterSetting("template","text/html","How are you")->
+
+addFilterSetting("dkim","use_from",1)->
+
+addFilterSetting("bypass_list_management","enable",1);
+
 echo $sendgrid->smtp->send($mail);
 ?>
