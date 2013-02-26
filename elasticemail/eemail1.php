18c0687fbff80d4739b9c1308781fb8061ebb541
Vibhu Tiwari <vibhutiwari321@gmail.com>
2013-02-26 10:49:44 +0530

    Update elasticemail/eemail1.php

--- elasticemail/eemail1.php
+++ elasticemail/eemail1.php
@@ -6,7 +6,7 @@ $csv .= '"vibhutiwari321@gmail.com","Mr","Alexander","Smith"'."\n";
 $csv .= '"vibhutiwari92@yahoo.in","Miss","Sarah","Dupon"'."\n";
 
 $text = 'Hello {Title} {LastName}, your first name is {FirstName}.';
-
-$res = $ee->mailMerge($csv, "vibhutiwari321@gmail.com", "Demo", "Demo Mail Merge", $text);
+$html='<body>Unsubscribe <a href="{unsubscribe}">Here</a></body>';
+$res = $ee->mailMerge($csv, "vibhutiwari321@gmail.com", "Demo", "Demo Mail Merge",$text,$html);
 echo $res;
 ?>
