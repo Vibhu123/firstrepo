87ff212cd1de01a44cae809098cfb40c5a83571f
Vibhu Tiwari <vibhutiwari321@gmail.com>
2013-03-05 17:04:52 +0530

    Update DefaultController.php

--- Symfony/src/Acme/StoreBundle/Controller/DefaultController.php
+++ Symfony/src/Acme/StoreBundle/Controller/DefaultController.php
@@ -7,19 +7,32 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     public function createAction(){
+        
 $prod=new Prod();
+
 $prod->setName('First Product');
+
 $prod->setPrice('25');
+
 $prod->setDescription('The first product of the database');
+
 $em=$this->getDoctrine()->getManager();
+
 $em->persist($prod);
-$em->flush();vi 
+
+$em->flush();
+
 return new Response('Created product id of first product'.$prod->getId());
+
 } 
 public function showAction($id){
+    
 $prod=$this->getDoctrine()->getRepository('AcmeStoreBundle:Prod')->find($id);
+
 if(!$prod){
+    
 throw $this->createNotFoundException('No product found for id'.$id);
+
 }
 }
     public function indexAction($name)
